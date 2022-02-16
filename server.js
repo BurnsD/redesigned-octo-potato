b7eb6ea1cb459680714070b1314469ad9c1efb29
BurnsD <Dustinburnsactech@gmail.com>
2022-02-15 21:33:30 -0600

    start of server.js/comments for functions needed

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+require('dotenv').config();
+
+// Start Application
+connection.connect(function(err){
+    if (err) throw err;
+    start();
+  })
+  
+// Show all options 
+
+  // viewEmployees
+
+  // viewDepartments
+
+  //viewRoles
+
+  // addEmployee
+
+  // addDepartment
+
+  // addRole
+
+  // updateRole
